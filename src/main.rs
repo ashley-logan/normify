87d80c36321d3ce16257774d2caef1ed8f604f16
ashley-logan <ashley-314159@outlook.com>
2025-10-27 01:19:46 -0500

    restructed api

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
+mod normalizer;
+use normalizer::NormifyContext;
 use serde_json::Value;
 use std::fs;
 use std::io::Read;
-mod normalizer;
-use normalizer::Normalizer;
-use std::collections::HashMap;
 
 fn main() {
     let mut file = fs::File::open("./tests/test_file.json").expect("couldn't open file");
@@ -12,9 +11,10 @@ fn main() {
         .expect("Unable to read json file");
     let payload: Value =
         serde_json::from_str(&json_content).expect("Could not parse json to Value variant"); // use serde-json to get the json_str as a Value variant
-    let mut context = Normalizer::new(); // initialize the context for normalization
-    context.normify_json(payload).expect("error normify"); // normalize
-    let tables: HashMap<String, HashMap<String, Vec<Value>>> = context.get_tables(); // get the tables as a hashmap of table names to tables
-    // println!("{:?}", tables);
-    // println!("{:?}", payload);
+    let data: NormifyContext =
+        NormifyContext::from_value(payload).expect("error parsing data from paylaod");
+    // println!("{:?}", data);
+    for (name, table) in data.tables {
+        println!("Table name:\n {}\n Table data:\n {:?}", name, table.records);
+    }
 }
--- src/normalizer.rs
+++ src/normalizer.rs
@@ -1,185 +1,181 @@
-use serde_json::{Error, Map, Value};
-use std::collections::HashMap;
+use anyhow::Result;
+use indexmap::IndexMap;
+use polars::{frame::DataFrame, prelude::DataType};
+use serde_json::{Map, Value};
 use uuid::Uuid;
 
-type NestedRecords = Vec<Map<String, Value>>; // a table as a list of rows
-type UnnestedRecords = HashMap<String, Vec<Value>>; // a table as a map of column names to columns
+type Row = Map<String, Value>; // row type is a map of field name to data element
 
+#[derive(Debug)]
 pub struct TableData {
-    rows: NestedRecords,
+    pub records: Vec<Row>, // represents the records as a vector of rows
+    schema: IndexMap<String, DataType>,
+}
+#[derive(Debug)]
+pub struct Relationship {
+    parent: Option<String>,   // holds the table name of the parent table
+    child: Option<String>,    // holds the table name of the child table
+    fk_field: Option<String>, // holds the field name of the foreign key
+}
+
+#[derive(Debug)]
+pub struct NormifyContext {
+    pub tables: IndexMap<String, TableData>,
+    relationships: Vec<Relationship>,
+}
+
+#[derive(Debug)]
+pub struct NormfiedData {
+    dataframes: IndexMap<String, DataFrame>, // maps dataframe name to DataFrame
+    relationships: IndexMap<String, Relationship>, // map dataframe name to Relationship struct
 }
 
 impl TableData {
     pub fn new() -> Self {
-        TableData { rows: vec![] }
-    }
-    pub fn transform(&self) -> UnnestedRecords {
-        let mut table: UnnestedRecords = HashMap::new();
-        for row in &self.rows {
-            for (key, value) in row {
-                table
-                    .entry(key.to_string())
-                    .or_insert(vec![])
-                    .push(value.clone());
-            }
+        TableData {
+            records: Vec::new(),
+            schema: IndexMap::new(),
         }
-        table
     }
-
-    pub fn append_row(&mut self, key: String, value: Value) {
-        let mut new_row = Map::new();
-        new_row.insert(key, value);
-        self.rows.push(new_row);
+    pub fn append_row(&mut self, row: Row) {
+        self.records.push(row);
     }
 }
-#[derive(Debug)]
-pub struct Normalizer {
-    // a map of table names to unformatted tables
-    tables: HashMap<String, UnnestedRecords>, // a map of table names to properly formatted tables
-}
 
-impl Normalizer {
+impl NormifyContext {
     pub fn new() -> Self {
-        Normalizer {
-            tables: HashMap::new(),
+        NormifyContext {
+            tables: IndexMap::new(),
+            relationships: Vec::new(),
         }
     }
 
-    pub fn append_table(&mut self, table: &TableData, table_name: &str) {
-        self.tables
-            .insert(table_name.to_string(), table.transform());
-    }
-
-    // pub fn append_row(&mut self, row: Map<String, Value>, table_name: &str) {
-    //     self.tables
-    //         .entry(table_name.to_string())
-    //         .or_insert(HashMap::new())
-    //         .push(row);
-
-    //     if let Some(table) = self.row_wise_tables.get_mut(table_name) {
-    //         // if table already exists in tables, add row to the table
-    //         table.push(row);
-    //     } else {
-    //         // else create a new table with the first row and insert it
-    //         self.row_wise_tables
-    //             .insert(table_name.to_string(), vec![row]);
-    //     }
-    // }
     pub fn parse_object(
         &mut self,
         table_name: &str,
         obj: &Map<String, Value>,
-        parent_id: Option<&str>,
-        parent_table: Option<&str>,
-    ) -> Result<(), Error> {
+        id: Option<&Value>,
+        id_field_name: Option<&str>,
+    ) -> Result<()> {
         println!("Creating table {}", table_name);
-        let mut new_table = TableData::new();
-        let new_id = Uuid::new_v4().to_string();
-        new_table.append_row("id".to_string(), Value::String(new_id.to_string()));
+        if let (Some(curr_id), Some(id_field)) = (id, id_field_name) {}
+
+        let mut new_row: Row = Map::new();
+        let curr_id = Value::String(Uuid::new_v4().to_string());
+        new_row.insert("id".to_string(), curr_id.clone());
 
         if let (Some(pid), Some(ptable)) = (parent_id, parent_table) {
-            new_table.append_row(format!("{}_id", ptable), Value::String(pid.to_string()));
+            new_row.insert(format!("{}_id", ptable), pid.clone());
         }
+
         for (k, v) in obj {
+            println!("processing item {}", k);
             // process each value in the object
             match v {
                 // if value is primitive type
                 Value::Null => {
-                    new_table.append_row(k.to_string(), Value::Null);
+                    new_row.insert(k.to_string(), Value::Null);
                 }
                 Value::Bool(b) => {
-                    new_table.append_row(k.to_string(), Value::Bool(b.clone()));
+                    new_row.insert(k.to_string(), Value::Bool(b.clone()));
                 }
                 Value::Number(n) => {
-                    new_table.append_row(k.to_string(), Value::Number(n.clone()));
+                    new_row.insert(k.to_string(), Value::Number(n.clone()));
                 }
                 Value::String(s) => {
-                    new_table.append_row(k.to_string(), Value::String(s.to_string()));
+                    new_row.insert(k.to_string(), Value::String(s.to_string()));
                 }
                 Value::Object(child) => {
                     println!("object {} is a new table", k);
+                    let child_id: Value = Value::String(Uuid::new_v4().to_string());
+                    let child_field: String = format!("{}_id", k);
+                    let new_table_name: String = format!("{}_table", k);
+                    new_row.insert(child_field, child_id);
                     // recursive call to self with table name as the key
                     self.parse_object(
-                        format!("{}_table", k).as_str(), // creates a new table name based on the key of the child object
-                        child,                           // child object becomes target object
-                        Some(new_id.as_str()),           // current id becomes parent id
-                        Some(table_name), // current table name becomes parent table name
+                        new_table_name.as_str(), // creates a new table name based on the key of the child object
+                        child,                   // child object becomes target object
+                        Some(child_id),          // current id becomes parent id
+                        Some(table_name),        // current table name becomes parent table name
                     )?;
                 }
                 Value::Array(arr) => {
                     println!("key {}'s value is an array", k);
-                    self.parse_array(
-                        table_name,
-                        &mut new_table,
-                        k.clone(),
-                        arr,
-                        parent_id,
-                        parent_table,
-                    )?;
+                    if arr.iter().all(|ele| ele.is_object()) {
+                        self.parse_objects_array(
+                            table_name,
+                            Some(k.to_string()),
+                            Some(&curr_id),
+                            &arr,
+                        )?;
+                    } else {
+                        self.parse_values_array(&mut new_row, k.to_string(), arr.to_vec())?;
+                    }
                 }
             };
-            self.append_table(&new_table, table_name);
+        }
+        let curr_table: &mut TableData = self
+            .tables
+            .entry(table_name.to_string())
+            .or_insert_with(TableData::new);
+        curr_table.append_row(new_row);
+        Ok(())
+    }
+
+    pub fn parse_values_array(
+        &mut self,
+        curr_row: &mut Row,
+        key: String,
+        arr: Vec<Value>,
+    ) -> Result<()> {
+        if arr.is_empty() {
+            curr_row.insert(key, Value::Null);
+        } else if arr.iter().any(|ele| ele.is_object()) {
+            panic!("found an object in an array of values with key {}", key);
+        } else {
+            curr_row.insert(key, Value::Array(arr));
         }
         Ok(())
     }
 
-    pub fn parse_array(
+    pub fn parse_objects_array(
         &mut self,
         table_name: &str,
-        curr_table: &mut TableData,
-        key: String,
+        key: Option<String>,
+        row_id: Option<&Value>,
         arr: &Vec<Value>,
-        parent_id: Option<&str>,
-        parent_table: Option<&str>,
-    ) -> Result<(), Error> {
-        if arr.is_empty() {
-            return Ok(());
-        }
-        if arr.iter().all(|item| item.is_object()) {
-            println!(
-                "{} is an array of objects, creating table name {}",
-                key, table_name
-            );
-            let new_name: String = format!("{}_table", key);
-            // if array is an array of objects, process each
-            for value in arr {
-                self.parse_object(
-                    new_name.as_str(),
-                    value.as_object().unwrap(),
-                    parent_id,
-                    parent_table,
-                )?;
+    ) -> Result<()> {
+        if key.is_none() {
+            for obj in arr {
+                self.parse_object(table_name, obj.as_object().unwrap(), None, None)?;
             }
         } else {
-            // else insert the array as the value for the row
-            curr_table.append_row(key, Value::Array(arr.to_owned()));
+            let new_table_name: String = format!("{}_table", key.unwrap());
+            for obj in arr {
+                self.parse_object(
+                    new_table_name.as_str(),
+                    obj.as_object().unwrap(),
+                    row_id,
+                    Some(table_name),
+                )?;
+            }
         }
         Ok(())
     }
 
-    pub fn normify_json(&mut self, payload: Value) -> Result<(), Error> {
+    pub fn from_value(payload: Value) -> Result<NormifyContext> {
+        let mut context: NormifyContext = NormifyContext::new();
         match payload {
             Value::Object(obj) => {
-                self.parse_object("root", &obj, None, None)?;
+                context.parse_object("ROOT", &obj, None, None)?;
             }
             Value::Array(arr) => {
-                self.parse_array(
-                    "root",
-                    &mut TableData::new(),
-                    "entry-array".to_string(),
-                    &arr,
-                    None,
-                    None,
-                )?;
+                context.parse_objects_array("ROOT", None, None, &arr)?;
             }
             _ => {
                 panic!("Neither Object nor Array found at root of JSON");
             }
         }
-        Ok(())
-    }
-
-    pub fn get_tables(self) -> HashMap<String, UnnestedRecords> {
-        self.tables
+        Ok(context)
     }
 }
